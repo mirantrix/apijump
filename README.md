bf03e228a215028ea95e30da84c078ad1763dfb5
User <mirantrix80@gmail.com>
2019-04-13 16:00:23 -0700

    [Feature]: Initial Setup

---
+++ README.md
@@ -0,0 +1,8 @@
+## Name
+
+Next FC Web App
+
+
+## Description
+
+The Next FC Web App shows the upcoming matches of you favorite teams.
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const formData = require('express-form-data');
+const os = require('os');
+const methodOverride = require('method-override');
+const dotenv = require('dotenv').config();
+const session = require('express-session');
+
+const app = express();
+const port = process.env.PORT || process.env.PORT_LOCAL;
+
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true })
+  .then(db => console.log(`Mongo DB: 200 ok`))
+  .catch(err => console.log(`ERROR: ${err}`));
+
+const options = {
+  uploadDir: os.tmpdir(),
+  autoClean: true,
+};
+
+const sessionMiddleware = session({
+	secret: String(process.env.SESSION_SECRET),
+	resave: false,
+	saveUninitialized: false
+})
+
+app.use(sessionMiddleware);
+app.use(formData.parse(options));
+app.use(formData.union());
+app.use(methodOverride('_method'));
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+
+app.listen(port, () => console.log(`Port: ${port}`));
